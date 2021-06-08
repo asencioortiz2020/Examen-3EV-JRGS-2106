c348bbc95cacb8000e83fed37a055c4fdd701718
Asencio Ortiz <ortiz19920@iesmarenostrum.com>
2021-06-08 20:34:15 +0200

    examen finalizado

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -2,71 +2,151 @@
 
 namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    /// <summary>
+    /// En la clase vamos a introducir un conjunto de notas y vemos la nota media,suspensos,aprobados,etc
+    /// </summary>
+    public class ConjuntoNotas  // esta clase nos calcula las estadísticas de un conjunto de notas 
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
 
-        public double media; // Nota media
+        private int suspensos;  // Suspensos         //ASEOR2021
+        private int aprobados;  // Aprobados
+        private int notables;  // Notables
+        private int sobresalientes;  // Sobresalientes
 
+        private double Notamedia; // Nota media
+
+        private const string listaVacia = "No hay nada";
+        private const string notasIncorrectas = "las notas introducidas no son válidas";    //ASEOR2021
+
+        /// <summary>
+        /// El constructor está vacío y se incializan los tipos de nota y la notaMedia a 0
+        /// </summary>
+        /// <remarks>No es necesariopasarle parámetros al constructor</remarks>
         // Constructor vacío
-        public estNot()
+        public ConjuntoNotas()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            suspensos = aprobados = notables = sobresalientes = 0;  // inicializamos las variables
+            Notamedia = 0.0;
+
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        /// <summary>
+        /// A este método le pasamos una lista con notas y nos dice el tipo de nota
+        /// </summary>
+        /// <param name="listaNotas">Este método tiene como parámetro una lista de notas</param>
+
+        public ConjuntoNotas(List<int> listaNotas)
         {
-            media = 0.0;
-
-            for (int i = 0; i < listnot.Count; i++)
+            Notamedia = 0.0;
+            //ASEOR2021
+            for (int i = 0; i < listaNotas.Count; i++)
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+                if (listaNotas[i] < 5)
+                {
+                    suspensos++;              // Por debajo de 5 suspenso
+                }
+                else if (listaNotas[i] > 5 && listaNotas[i] < 7)
+                {
+                    aprobados++;// Nota para aprobar: 5
+                }
+                else if (listaNotas[i] > 7 && listaNotas[i] < 9)
+                {
+                    notables++;// Nota para notable: 7
+                }
+                else if (listaNotas[i] > 9)
+                {
+                    sobresalientes++;         // Nota para sobresaliente: 9
+                }
 
-                media = media + listnot[i];
+
+                Notamedia = Notamedia + listaNotas[i];
+
+
+
             }
 
-            media = media / listnot.Count;
+            Notamedia = Notamedia / listaNotas.Count;
         }
 
 
         // Para un conjunto de listnot, calculamos las estadísticas
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
         //
-        // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
-        {                                 
-            media = 0.0;
+        // El método devuelve -1 si ha habido algún problema, la media en caso contrario
+        /// <summary>
+        /// A este método se le pasa una lista de notas y nos devuelve la nota media
+        /// </summary>
+        /// <param name="listnot"></param>
+        /// <returns>Esta función nos devuelve un double</returns>
+        public double calculaMedia(List<int> listnot)
+        {
+            Notamedia = 0.0;
 
             // TODO: hay que modificar el tratamiento de errores para generar excepciones
             //
             if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
+            {
+                throw new System.ArgumentOutOfRangeException(listaVacia);
 
-            for (int i=0;i<10;i++)
-                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
+            }
+
+            //return -1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (listnot[i] < 0 || listnot[i] > 10)
+                {
+                    throw new System.ArgumentOutOfRangeException(notasIncorrectas);
+                }
+            }
+            // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+            //return -1;
 
             for (int i = 0; i < listnot.Count; i++)
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+                if (listnot[i] < 5)
+                {
+                    suspensos++;              // Por debajo de 5 suspenso
+                }
 
-                media = media + listnot[i];
+                else if (listnot[i] >= 5 && listnot[i] < 7)
+                {
+                    aprobados++;// Nota para aprobar: 5
+                }
+
+                else if (listnot[i] >= 7 && listnot[i] < 9)
+                {
+                    notables++;// Nota para notable: 7
+                }
+
+                else if (listnot[i] > 9)
+                {
+                    sobresalientes++;         // Nota para sobresaliente: 9
+                }
+
+
+                Notamedia = Notamedia + listnot[i];
             }
 
-            media = media / listnot.Count;
+            Notamedia = Notamedia / listnot.Count;
 
-            return media;
+            return Notamedia;
+        }
+        public bool AprobadosySuspensos(List<int> listnot, double notaMedia)
+        {
+
+            bool res = false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (notaMedia >= 5)
+                {
+                    res = true;
+                }
+            }
+
+            return res;
         }
     }
 }
--- ClassLibrary1/UnitTestProject1/UnitTest1.cs
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -5,7 +5,7 @@ using Examen3EV_NS;
 namespace UnitTestProject1
 {
     [TestClass]
-    public class UnitTest1
+    public class Examen3EvTests
     {
         [TestMethod]
         public void TestMethod1()
@@ -25,6 +25,82 @@ namespace UnitTestProject1
             int aprE = 1;
             int notE = 2;
             int sbrE = 1;
+
+            for (int i = 0; i < notas.Count; i++)
+            {
+                Assert.AreEqual(mediaEsperada, notas[i]);
+            }
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void prueba_listaNotas_vacia_devuelveExcepcion()
+        {
+            List<int> notas = new List<int>();
+            ConjuntoNotas nota = new ConjuntoNotas();
+
+            //Probamos que la lista tiene un tamaño menor que 0 // NO VÁLIDO
+            notas.Add(-1);
+            nota.calculaMedia(notas);
+
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void prueba_listaNotas_Incorrectas_devuelevExcepcion()
+        {
+            List<int> notas = new List<int>();
+            ConjuntoNotas nota = new ConjuntoNotas();
+
+            //Probamos que la lista tiene un tamaño menor que 0 // NO VÁLIDO
+            notas.Add(60);
+            nota.calculaMedia(notas);
+
+
+        }
+        [TestMethod]
+        public void prueba_notasValidas()
+        {
+            List<int> notas = new List<int>();
+
+            notas.Add(5);
+            notas.Add(5);
+            notas.Add(5);
+
+
+            double mediaEsperada = 5.00;
+
+
+            for (int i = 0; i < notas.Count; i++)
+            {
+                Assert.AreEqual(mediaEsperada, notas[i]);
+            }
+
+        }
+        [TestMethod]
+        public void prueba_Aprobados()
+        {
+
+            ConjuntoNotas nota = new ConjuntoNotas();
+
+            List<int> notas = new List<int>();
+
+            double mediaEsperada = 5.00;
+
+
+            notas.Add(4);
+            notas.Add(5);
+            notas.Add(5);
+
+            bool res = nota.AprobadosySuspensos(notas, mediaEsperada);
+            bool esperado = true;
+
+
+
+            Assert.AreEqual(esperado, res);
+
+
+
         }
     }
 }
